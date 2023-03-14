9f0850ee173a217b6d39e87994b99b2582de1255
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-03-14 00:01:04 -0400

    messaging screen

--- app/src/main/java/com/example/messagingapp/ChatActivity.kt
+++ app/src/main/java/com/example/messagingapp/ChatActivity.kt
@@ -1,11 +1,28 @@
 package com.example.messagingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var messageRecyclerView: RecyclerView
+    private lateinit var messageBox: EditText
+    private lateinit var sendButton: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val name = intent.getStringExtra("name")
+        val uid = intent.getStringExtra("uid")
+
+        supportActionBar?.title = name
+
+        messageRecyclerView = findViewById(R.id.messageRecyclerView)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sentButton)
     }
 }
